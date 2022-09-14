47d50534b1346ccf4a016427981f6ee5e5740afb
Naveenkumar <knaveenkumar2@mahindra.com>
2022-09-14 16:15:13 +0530

    added dto

--- entity/user.go
+++ entity/user.go
@@ -4,6 +4,6 @@ type User struct {
 	ID       uint64 `gorm:"primaryKey:auto_increment" json:"id"`
 	Name     string `gorm:"type:varchar(255)" json:"name"`
 	Email    string `gorm:"uniqueIndex;type:varchar(255)" json:"email"`
-	password string `gorm:"->;<-;not null" json:"-`
+	Password string `gorm:"->;<-;not null" json:"-"`
 	Token    string `gorm:"-" json:"_,omitempty"`
 }
